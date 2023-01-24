1570e227c5c265a808840977c908eca3ce0ae4e5
Alvaro Gonzalez <agonzalez@lsq.com>
2023-01-24 11:49:12 -0600

    add threads

--- hardware/main.py
+++ hardware/main.py
@@ -1,14 +1,18 @@
+import _thread
 import time
 
 import network
-from machine import ADC, Pin, SoftI2C, RTC
+import urequests
+from machine import ADC, RTC, Pin, SoftI2C
 
 import constants as Const
-from lib import wlan_client, oled_display as oled
-from workflows import monitor, calibration
+from lib import app_sensors as sensors
+from lib import oled_display as oled
+from lib import remote_time, wlan_client
+
 # from dht11 import DHT11
 
-# INITIALIZE PERIPHERALS ############################################################
+# INIT ------------------------------------------------------------------------------
 builtin_led = Pin(2, Pin.OUT)
 rtc = RTC()
 boot_btn = Pin(0, Pin.IN, Pin.PULL_DOWN)
@@ -20,44 +24,121 @@ ldr_adc = ADC(Pin(35), atten=ADC.ATTN_11DB)
 
 wlan.config(reconnects=5)
 display.fill(0)
-# ###################################################################################
+display.hline(0, 12 ,128, 2)
+# /INIT -----------------------------------------------------------------------------
 
-current_time = time.time()
-current_workflow = 'MONITOR'
-peripherals = {
-    'rtc': rtc,
-    'builtin_led': builtin_led,
-    'oled': display,
-    'ldr_adc': ldr_adc,
-    'soil_adc': soil_adc,
-    'button': boot_btn
-}
+# FUNCTIONS -------------------------------------------------------------------------
+def is_pressed(btn):
+    return not btn.value()
 
-monitorWorkflow = monitor.Workflow(peripherals, current_time)
-calibrationWorkflow = calibration.Workflow(peripherals)
+def show_calibration_menu():
+    display.text_line('Calibration', 1)
 
-def handle_exception(self, exc: Exception):
+def display_sensors(sensors: dict):
+    for i, value in enumerate(sensors.values()):
+        print(f"{value.get('name')}: {value.get('raw_value')}")
+        display.text_line(f"{value.get('name')}: {value.get('display_value')}", i+3)
+
+def handle_exception(exc: Exception):
     exc_as_str = str(exc)
     print(exc_as_str)
     for i, w in enumerate(exc_as_str.split(' ')):
-        self.oled.text_line(w, i + 3)
+        display.text_line(w, i + 3)
+# /FUNCTIONS ------------------------------------------------------------------------
+
+ldr = sensors.LdrSensor(ldr_adc)
+soil = sensors.SoilSensor(soil_adc)
+
+selected_sensor = ldr.name
+initial_time = time.time()
+current_workflow = Const.MONITOR_WORKFLOW
+
+def keep_posting_data(latest_request):
+    def try_connect(retries = 1, led = None):
+        retry_count = 0
+        # While network is available
+        #   and has remaining retries
+        while retry_count < retries and not wlan.isconnected() and wlan_client.is_nearby(wlan, Const.SSID):
+            display.text_line("Connecting...", 8)
+            wlan_client.connect(wlan, Const.SSID, Const.PSSW, indicator_gpio = led)
+            retry_count += 1
+        display.clear_lines([8])
+        # Check if connection succeded after retries
+        return wlan.isconnected()
+
+    def create_document(data: dict):
+        fields = {}
+        for key, value in data.items():
+            valueType='Value'
+            if type(value) is None:
+                valueType = 'null' + valueType
+            elif type(value) is bool:
+                valueType = 'boolean' + valueType
+            elif type(value) is int:
+                valueType = 'integer' + valueType
+            elif type(value) is float:
+                valueType = 'double' + valueType
+            elif type(value) is str:
+                valueType = 'string' + valueType
+            elif type(value) is list or type(value) is tuple:
+                valueType = 'array' + valueType
+                raise ValueError('array document fields are not yet supported')
+            elif type(value) is dict:
+                valueType = 'map' + valueType
+                fields[key] = { valueType: create_document(value) }
+                continue
+            fields[key] = { valueType: value }
+        return {'fields': fields}
+
+    def post_sensors_data(data):
+        return urequests.post(
+            f'https://firestore.googleapis.com/v1/projects/{Const.FIRESTORE_PROJECT_ID}/databases/(default)/documents/sensors',
+            json=data,
+            headers={
+                'Content-Type': 'application/json',
+                'key': Const.FIRESTORE_API_KEY
+            },
+        )
+   
+    while True:
+        now = time.time()
+        elapsed_time = now - latest_request
+        try:
+            if not try_connect(Const.MAX_RECONNECTS, builtin_led):
+                builtin_led.off()
+                display.text_line("Not connected...", 8)
+                print("Failed to reconnect...")
+            elif elapsed_time >= Const.SEND_REQUEST_SECS:
+                print("Sending data...")
+                display.text_line("Sending data...", 8)
+                documentData = create_document({
+                    'ldr': ldr.as_dict(),
+                    'soil': soil.as_dict()
+                })
+                current_time = remote_time.get_current_time(Const.TIMEZONE).get('datetime')
+                documentData['fields']['created_at'] = {'timestampValue': current_time}
+                print(documentData)
+                post_sensors_data(documentData).close()
+                display.clear_lines([8])
+                latest_request = now
+            time.sleep(1)
+        except Exception as exc:
+            handle_exception(exc)
+            time.sleep(5)
+            display.clear_lines(range(1, 9))
+
+_thread.start_new_thread(keep_posting_data, (initial_time,))
 
 while True:
-    display.text_line(Const.PROJECT_NAME, 1)
-    display.hline(0, 12 ,128, 2)
+    if is_pressed(boot_btn):
+        current_workflow = Const.CALIBRATION_WORKFLOW
 
-    try:
-        if not boot_btn.value(): # values are inverted
-            current_workflow = 'CALIBRATION'
-
-        if current_workflow == 'MONITOR':
-            monitorWorkflow.run()
-        elif current_workflow == 'CALIBRATION':
-            calibrationWorkflow.run()
-
-        time.sleep_ms(100)
-
-    except Exception as exc:
-        handle_exception(exc)
-        time.sleep(5)
-        display.clear_lines(range(1, 9))
+    if current_workflow is Const.MONITOR_WORKFLOW:
+        display.text_line("Plantagotchi", 1)
+        display_sensors({
+            'ldr': ldr.as_dict(),
+            'soil': soil.as_dict()
+        })
+        time.sleep(1)
+    if current_workflow is Const.CALIBRATION_WORKFLOW:
+        show_calibration_menu()
